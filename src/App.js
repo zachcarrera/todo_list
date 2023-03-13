14a27fcd1fa0d50b25260b34506f1631b1daa458
Zach Carrera <zachcarrera@gmail.com>
2023-03-12 18:26:34 -0700

    add todo list functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const starterTodo = [
+    { text: "walk the dog", isCompleted: false },
+    { text: "feed the cat", isCompleted: false },
+    { text: "take out the trash", isCompleted: false },
+    { text: "wash the car", isCompleted: false },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todoList, setTodoList] = useState(starterTodo);
+
+    const [todoForm, setTodoForm] = useState("");
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newTodo = [{ text: todoForm, isCompleted: false }, ...todoList];
+        setTodoList(newTodo);
+        setTodoForm("");
+    };
+
+    const handleComplete = (completedIndex) => {
+        const newTodo = todoList.map((todo, index) => {
+            const indvidualTodo = { ...todo };
+            if (index === completedIndex) {
+                indvidualTodo.isCompleted = !indvidualTodo.isCompleted;
+            }
+            return indvidualTodo;
+        });
+        setTodoList(newTodo);
+    };
+
+    const handleDelete = (deleteIndex) => {
+        const newTodo = todoList.filter((todo, index) => {
+            return deleteIndex !== index;
+        });
+        setTodoList(newTodo);
+    };
+    return (
+        <div className="App">
+            <h1>Todo List</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="todoName"
+                    value={todoForm}
+                    onChange={(e) => setTodoForm(e.target.value)}
+                />
+                <input type="submit" value="Add" />
+            </form>
+            <ul>
+                {todoList.map((todo, index) => (
+                    <li key={index}>
+                        <span>{todo.text}</span>
+                        <input
+                            type="checkbox"
+                            name="completed"
+                            checked={todo.isCompleted}
+                            onChange={(e) => handleComplete(index)}
+                        />
+                        <button onClick={(e) => handleDelete(index)}>
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
